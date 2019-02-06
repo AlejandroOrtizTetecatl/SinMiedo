12c8e235bd8b9f2c290cd35631cb42256786ef6c
AlejandroOrtizTetecatl <31837656+AlejandroOrtizTetecatl@users.noreply.github.com>
2019-02-06 00:16:31 -0600

    Login
    Primer commit con el login creado

---
+++ SinMiedos/SinMiedos/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace SinMiedos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar(object sender, RoutedEventArgs e)
+        {
+            String user = Usuario.Text;
+            String contra = Contrasenia.Password;
+           // MessageBox.Show(contra);
+
+            VentanMenu menu = new VentanMenu();
+            menu.Owner = this;
+            menu.Show();
+            this.Hide();
+
+        
+        }
+    }
+}
---
+++ SinMiedos/SinMiedos/VentanMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SinMiedos
+{
+    /// <summary>
+    /// Lógica de interacción para VentanMenu.xaml
+    /// </summary>
+    public partial class VentanMenu : Window
+    {
+        public VentanMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Salir(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            
+        }
+        private void Minimizar(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            
+
+        }
+        private void Maximizar(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+
+        }
+        private void Mover(object sender,MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
